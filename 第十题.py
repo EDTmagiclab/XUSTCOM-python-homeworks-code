f057b4b7a5322a1b9a1e8e57397ff9b284b751e9
EDTmagiclab <60107546+EDTmagiclab@users.noreply.github.com>
2021-04-12 00:08:14 +0800

    第十题答案，千万别直接抄，这题是我自己的风格

---
+++ 第十题.py
@@ -0,0 +1,15 @@
+dict1={}
+b=0
+for i in range (0,3):
+    a=str(input("名字"))
+    b=float(input("分数"))
+    dict1[a]=b
+a=dict1.values()
+a=list(a)
+a.sort()
+for i in a:
+    b=b+i
+    c=b/3
+print( "最高分%s分" % a[2])
+print("最底分%s分" % a[0])
+print("平均分%s分" % c)
